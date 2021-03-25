85de198f1b2aa3335a95744646ffbb4e06937742
OliverMata <78970451+OliverMata@users.noreply.github.com>
2021-03-24 23:02:37 -0400

    Add files via upload

---
+++ programa 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace programa_1
+{
+     class Empleados
+    {
+       private string[] nombre;
+       private float[] sueldo;
+
+        public void Cargar() 
+        {
+            nombre=new string[5];
+            sueldo=new float[5];
+            for(int x=0; x < nombre.Length; x++) 
+            {
+                Console.WriteLine();
+                Console.Write("Ingrese el nombre del empleado: ");
+                nombre[x] = Console.ReadLine();
+                Console.Write("Ingrese el sueldo: ");
+                string n;
+                n = Console.ReadLine();
+                sueldo[x]=float.Parse(n);
+            }
+        }
+
+        public void MayorSueldo() 
+        {
+            float mayor;
+            int pos;
+            mayor = sueldo[0];
+            pos = 0;
+            for(int f=1; f < nombre.Length; f++) 
+            {
+                if (sueldo[f] > mayor) 
+                {
+                    mayor = sueldo[f];
+                    pos=f;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("El empleado con sueldo mayor es " + nombre[pos]);
+            Console.WriteLine("Tiene un sueldo: " + mayor);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            Empleados obj = new Empleados();
+            obj.Cargar();
+            obj.MayorSueldo();
+        }
+    }
+}
+
